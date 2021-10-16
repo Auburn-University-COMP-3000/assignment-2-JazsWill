1be7f1ffc8d5aced68d1cc86ed502f8d02f1d282
jaw0119 <92323456+JazsWill@users.noreply.github.com>
2021-10-16 14:34:00 -0500

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -62,3 +62,44 @@ int getMonthValue(int month, int year)
     else
         return monthValue = 5;
 }
+
+int main()
+{
+    // Assignment 2 - Exercise 1
+
+         string dayOfWeek;
+         int month, day, year;
+
+         cout << "Assignment 2 - Exercise 1\n";
+
+         cout << "Think of a random day of the year.\n\n";
+         cout << "What was the month? (mm): ";
+         cin >> month;
+
+         cout << "What was the day of the month? (dd): ";
+         cin >> day;
+
+         cout << "What was the year? (yyyy): ";
+         cin >> year;
+         
+         int dayValue;
+         dayValue = (day + getMonthValue(month, year) + getYearValue(year) + getCenturyValue(year)) % 7;
+
+         if (dayValue == 0)
+             dayOfWeek = "Sunday";
+         else if (dayValue == 1)
+             dayOfWeek = "Monday";
+         else if (dayValue == 2)
+             dayOfWeek = "Tuesday";
+         else if (dayValue == 3)
+             dayOfWeek = "Wednesday";
+         else if (dayValue == 4)
+             dayOfWeek = "Thursday";
+         else if (dayValue == 5)
+             dayOfWeek = "Friday";
+         else if (dayValue == 6)
+             dayOfWeek = "Saturday";
+
+         cout << "\nThe date: " << month << "/" << day << "/" << year << " is on a " << dayOfWeek << ".\n\n";
+}
+
